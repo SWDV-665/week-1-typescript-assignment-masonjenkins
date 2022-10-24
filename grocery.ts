9e8a17620ecdc5587daf84c56fbf66add19c1789
Mason Jenkins <mason.jenkins@bayer.com>
2022-10-23 19:58:34 -0500

    complete

---
+++ grocery.ts
@@ -0,0 +1,28 @@
+
+class Grocery {
+    name: string 
+    quantity: number 
+    price: number 
+    category: string 
+
+    constructor(name: string, quantity: number, price: number, category: string) {
+        this.name = name 
+        this.quantity = quantity
+        this.price = price 
+        this.category = category
+    }
+}
+
+let groceries = [new Grocery('milk', 3, 4.5, 'dairy'), new Grocery('bread', 6, 2, 'bread'), new Grocery('eggs', 11, 3, 'eggs')]
+
+groceries.map(item => {
+    document.write('Name: ' + item.name)
+    document.write("<br>")
+    document.write('Quantity: ' + item.quantity)
+    document.write("<br>")
+    document.write('Price: ' + item.price)
+    document.write("<br>")
+    document.write('Category: ' + item.category)
+
+    document.write("<br> <br>")
+})
